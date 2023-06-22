ed2e94811ebf107b9c3431725e8d64e831315b41
sora-yuka <feelingjeez@gmail.com>
2023-06-22 22:19:22 +0600

    Revert "Initial commit"
    This reverts commit d18550ce5a7dc107f6d567ba79ecc23bd14d9e86.

--- src/auth/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class UserCreate(BaseModel):
-    email: str = Field(..., example="example@gmail.com")
-    username: str = Field(..., example="example")
-    hashed_password: str = Field(..., example="qwerty123")
-    

29b7a0183c4c84234d31ce27bf8b9fe8d223aa32
sora-yuka <feelingjeez@gmail.com>
2023-06-23 12:21:22 +0600

    working login

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
@@ -23,6 +24,14 @@ async def profile_register(user: schemas.UserCreate, session: AsyncSession = Dep
         """ Handling 'Existing user' exception """
         raise HTTPException(status_code=404, detail="User already registered.")
     
-@router.get("/profile")
-async def get_profile(email: str, session: AsyncSession = Depends(get_async_session)):
-    return await views.get_user(email, session)
+
+@router.post("/login")
+async def profile_login(format_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_async_session)):
+    result = await views.get_user(format_data.username, session)
+    if result.get("data") == []:
+        raise HTTPException(status_code=404, detail="User does not exists")
+    password = result.get("data")[0].get("hashed_password")
+    verified_password = verify_password(format_data.password, password)
+    if not verified_password:
+        raise HTTPException(status_code=404, detail="Incorrect email or password")
+    return format_data

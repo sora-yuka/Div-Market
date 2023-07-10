fb464a51130919af4d8bd7690a8eae61306a2d32
sora-yuka <feelingjeez@gmail.com>
2023-07-10 17:34:53 +0600

    product comments

--- src/applications/products/router.py
+++ src/applications/products/router.py
@@ -13,6 +13,7 @@ router = APIRouter(
 
 @router.get("/category/")
 async def get_category(session: AsyncSession = Depends(get_async_session)):
+    """ Getting category list """
     return await views.get_category(session=session)
 
 
@@ -20,16 +21,19 @@ async def get_category(session: AsyncSession = Depends(get_async_session)):
 async def create_category(
         title: str, current_user = Depends(get_current_user), session: AsyncSession = Depends(get_async_session)
     ):
+    """ Creating category for product """
     return await views.create_category(title=title, session=session)
 
 
 @router.delete("/delete_category/")
 async def delete_category(category_id: int, session: AsyncSession = Depends(get_async_session)):
+    """ Deleting category """
     return await views.delete_category(category_id=category_id, session=session)
 
 
 @router.get("/")
 async def get_product(current_user = Depends(get_current_user), session: AsyncSession = Depends(get_async_session)):
+    """ Getting product list """
     return await views.get_products(session=session)
 
 
@@ -38,6 +42,7 @@ async def detail_product(
         product_id: int, current_user = Depends(get_current_user),
         session: AsyncSession = Depends(get_async_session)
     ):
+    """ Getting detailed product """
     return await views.detail_product(product_id=product_id, current_user=current_user, session=session)
 
 
@@ -46,6 +51,7 @@ async def create_product(
         product: schemas.ProductCreate, current_user = Depends(get_current_user), 
         session: AsyncSession = Depends(get_async_session)
     ):
+    """ Creating product """
     return await views.create_product(product_item=product.dict(), current_user=current_user, session=session)
 
 
@@ -54,4 +60,5 @@ async def edit_product(
         product: schemas.ProductEdit, current_user = Depends(get_current_user),
         session: AsyncSession = Depends(get_async_session)
     ):
+    """ Editing product """
     return await views.edit_product(product_item=product.dict(), current_user=current_user, session=session)
